ed069b7fcff1a16c127503d024f85ae3f6edb6d7
daxx <daxx@gmail.com>
2025-12-25 17:29:21 +0530

    guess number file was upgraded

--- guess-number.py
+++ guess-number.py
@@ -1,14 +1,24 @@
 import random
 
 secret_number = random.randint(1, 10)
+attempts = 3
 
-guess = int(input("Guess a number between 1 and 10: "))
+print("🎮 Welcome to the Guessing Game!")
+print("You have 3 attempts.")
 
-if guess == secret_number:
-    print("🎉 Correct! You guessed it.")
-elif guess < secret_number:
-    print("Too low 😅")
-else:
-    print("Too high 😬")
+while attempts > 0:
+    guess = int(input("Guess a number between 1 and 10: "))
 
-print("The number was:", secret_number)
+    if guess == secret_number:
+        print("🎉 Correct! You won!")
+        break
+    elif guess < secret_number:
+        print("Too low 😅")
+    else:
+        print("Too high 😬")
+
+    attempts -= 1
+    print("Attempts left:", attempts)
+
+if attempts == 0:
+    print("😢 You lost. The number was:", secret_number)
